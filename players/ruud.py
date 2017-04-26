3fb2cdf75cbd53680dd01d138f171caf62f65499
Ruud van Asseldonk <dev@veniogames.com>
2017-04-26 15:01:00 +0200

    Imlement my player (Ruud)
    Strategy: move towards finish if we are next to it. Move towards nearest
    unexplored position otherwise.

--- players/ruud.py
+++ players/ruud.py
@@ -1,21 +1,106 @@
 from game import moves
-from game.mazefield_attributes import Path, Finish
+from game.mazefield_attributes import Start, Path, Finish, Wall
 from players.player import Player
+from heapq import heappush, heappop
+from os import abort
+
+directions = [moves.RIGHT, moves.UP, moves.LEFT, moves.DOWN]
+
+
+def look_at(surroundings, direction):
+    if direction == moves.RIGHT:
+        return surroundings.right
+    elif direction == moves.UP:
+        return surroundings.up
+    elif direction == moves.LEFT:
+        return surroundings.left
+    elif direction == moves.DOWN:
+        return surroundings.down
+
+
+def directions_where(surroundings, expected):
+    return [d for d in directions if look_at(surroundings, d) is expected]
+
+
+def offset(direction):
+    if direction == moves.RIGHT:
+        return (1, 0)
+    elif direction == moves.UP:
+        return (0, 1)
+    elif direction == moves.LEFT:
+        return (-1, 0)
+    elif direction == moves.DOWN:
+        return (0, -1)
+
+
+def add(p, q):
+    (px, py) = p
+    (qx, qy) = q
+    return (px + qx, py + qy)
 
 
 class Ruud(Player):
-    name = "SamplePlayer"
+    name = "Ruud"
+
+    def __init__(self):
+        self.maze = {
+            (0, 0): Start
+        }
+        self.position = (0, 0)
+        self.current_route = []
+
+
+    def observe(self, surroundings):
+        for d in directions:
+            self.maze[add(self.position, offset(d))] = look_at(surroundings, d)
+
+    def route_to_nearest_unknown(self):
+        heap = [(0, self.position)]
+        done = {
+            self.position: self.position
+        }
+        done_moves = {}
+        goal = None
+
+        while heap:
+            cost, pos = heappop(heap)
+            for d in directions:
+                dpos = add(pos, offset(d))
+                at = self.maze.get(dpos)
+
+                if dpos in done:
+                    continue
+
+                done[dpos] = pos
+                done_moves[dpos] = d
+
+                if at is None:
+                    goal = dpos
+                    break
+                elif at is Path:
+                    heappush(heap, (cost + 1, dpos))
+
+            if goal:
+                break
+
+        assert goal is not None, 'Maze must have exit.'
+
+        route = []
+        while goal != self.position:
+            route.append(done_moves[goal])
+            goal = done[goal]
+
+        return route
 
     def turn(self, surroundings):
-        if surroundings.right in [Path, Finish]:
-            return moves.RIGHT
+        # If we can reach the finish, go there.
+        for d in directions_where(surroundings, Finish):
+            return d
 
-        if surroundings.left == Path:
-            return moves.LEFT
+        # Look around, store what we learned.
+        self.observe(surroundings)
 
-        if surroundings.up == Path:
-            return moves.UP
-
-        if surroundings.down == Path:
-            return moves.DOWN
-
+        # Move towards the nearest unknown position.
+        move = self.route_to_nearest_unknown().pop()
+        self.position = add(self.position, offset(move))
+        return move
